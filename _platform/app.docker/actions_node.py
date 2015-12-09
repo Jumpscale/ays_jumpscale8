07734fa5eb6acd5b2e9ba53bafdc52e7dd89f54b
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2015-12-09 11:49:59 +0400

    app.docker: move install logic in actions_node

--- _platform/app.docker/actions_node.py
+++ _platform/app.docker/actions_node.py
@@ -4,4 +4,16 @@ ActionsBase = j.atyourservice.getActionsBaseClassNode()
 
 
 class Actions(ActionsBase):
-    pass
+
+    def install(self, serviceObj):
+        self.installDocker(serviceObj)
+
+    def installDocker(self, serviceObj):
+        cuisine = j.tools.cuisine.local
+        # install docker
+        codename = cuisine.run('lsb_release -sc')
+        cuisine.dir_ensure("/etc/apt/sources.list.d")
+        source = 'deb https://apt.dockerproject.org/repo ubuntu-%s main' % codename
+        cuisine.file_write("/etc/apt/sources.list.d/docker.list", source)
+        cuisine.run('apt-get update;apt-cache policy docker-engine; apt-get install -y docker-engine')
+        cuisine.run('service docker start')
