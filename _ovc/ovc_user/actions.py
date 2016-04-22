91c4378bf9169fc8223db56d93f49cd4b209d8b6
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-04-22 13:21:36 +0200

    add ovc_user

---
+++ _ovc/ovc_user/actions.py
@@ -0,0 +1,33 @@
+
+from JumpScale import j
+
+
+class Actions():
+
+    def init(self):
+        pass
+
+    def install(self):
+        client = self.service.actions.getClient()
+        # create vdc if it doesn't exists
+        if not client.api.system.usermanager.userexists(name='$(username)'):
+            groups = self.service.hrd.getList('groups')
+            client.api.system.usermanager.create(username='$(username)', password='$(password)', groups=groups, emails=['$(email)'], domain='')
+        # authorize user to all consumed vdc
+        for vdc in self.service.producers['vdc']:
+            acc = client.account_get(vdc.hrd.get('g8.account'))
+            space = acc.space_get(vdc.instance, vdc.hrd.get('g8.location'))
+            client.api.cloudapi.cloudspaces.addUser(cloudspaceId=space.id, userId='$(username)', accesstype="ARCXDU")
+
+    def uninstall(self):
+        # unauthorize user to all consumed vdc
+        for vdc in self.service.producers['vdc']:
+            acc = client.account_get(vdc.hrd.get('g8.account'))
+            space = acc.space_get(vdc.instance, vdc.hrd.get('g8.location'))
+            client.api.cloudapi.cloudspaces.deleteUser(cloudspaceId=space.id, userId='$(username)', recursivedelete=True)
+
+    def getClient(self):
+        clientname = """$(producer.g8client)"""
+        clientname = clientname.strip().strip("',")
+        g8client = j.atyourservice.getServiceFromKey(clientname)
+        return g8client.actions.getClient()
