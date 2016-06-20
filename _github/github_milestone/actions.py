11552b35fc78ab7645d6e8ce8e4a109a37db17b8
Muhamad Azmy <muhamad.azmy@codescalers.com>
2016-06-20 10:22:47 +0200

    Move github templates to github_automation

--- _github/github_milestone/actions.py
+++
@@ -1,9 +0,0 @@
-from JumpScale import j
-
-
-class Actions(ActionsBaseMgmt):
-    def input(self,service,recipe,role,instance,args={}):
-        if "milestone.title" not in args:
-            args['milestone.title']=instance
-
-        return args
