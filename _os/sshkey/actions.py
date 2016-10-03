14cddb1c4aad4ccd79d8198969a26c0707589e8b
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-03 17:20:10 +0200

    fix dns client

--- _os/sshkey/actions.py
+++ _os/sshkey/actions.py
@@ -1,77 +1,75 @@
-from JumpScale import j
+def getSSHKey(service):
+    from JumpScale import j
+    keydest = j.sal.fs.joinPaths(service.path, "sshkey_%s" % service.instance)
+    privkey = j.sal.fs.fileGetContents(keydest)
+    pubkey = j.sal.fs.fileGetContents(keydest + ".pub")
+    return privkey, pubkey
 
 
-class Actions(ActionsBaseMgmt):
+def init(service):
+    """
+    create key
+    """
+    from JumpScale import j
+    if service.hrd.get("key.name") == "":
+        service.hrd.set("key.name", service.instance)
 
-    def getSSHKey(self, service):
-        keydest = j.sal.fs.joinPaths(service.path, "sshkey_%s"%service.instance)
-        privkey = j.sal.fs.fileGetContents(keydest)
-        pubkey = j.sal.fs.fileGetContents(keydest + ".pub")
-        return privkey, pubkey
+    name = service.hrd.get("key.name")
 
-    def init(self, service):
-        """
-        create key
-        """
-        if service.hrd.get("key.name") == "":
-            service.hrd.set("key.name", service.instance)
+    tmpdir = j.sal.fs.getTmpDirPath()
 
-        name = service.hrd.get("key.name")
-
-        tmpdir = j.sal.fs.getTmpDirPath()
-
-        if not j.do.checkSSHAgentAvailable():
-            j.do._loadSSHAgent()
-
-        if j.do.getSSHKeyPathFromAgent(name, die=False)!=None:
-            keyfile = j.do.getSSHKeyPathFromAgent(name)
-        elif service.hrd.get("key.path") != "":
-            keyfile = service.hrd.get("key.path")
-        else:
-            keyfile=j.sal.fs.joinPaths(tmpdir,name)
-            cmd = "ssh-keygen -t rsa -f %s -P '%s' " % (keyfile, service.hrd.getStr('key.passphrase'))
-            print(cmd)
-            j.sal.process.executeWithoutPipe(cmd)
-
-        if not j.sal.fs.exists(keyfile):
-            raise j.exceptions.Input("Cannot find ssh key location:%s"%keyfile)
-
-        keydest = j.sal.fs.joinPaths(service.path, "sshkey_%s"%service.instance)
-        if not keyfile == keydest:
-            service.hrd.set("key.path", keydest)
-            j.sal.fs.copyFile(keyfile,keydest)
-            j.sal.fs.copyFile(keyfile+".pub",keydest+".pub")
-
-        privkey = j.sal.fs.fileGetContents(keydest)
-        pubkey = j.sal.fs.fileGetContents(keydest + ".pub")
-
-        service.hrd.set('key.pub', pubkey)
-        service.hrd.set('key.priv', privkey)
-
-        j.sal.fs.chmod(keydest, 0o600)
-        j.sal.fs.chmod(keydest+".pub", 0o600)
-
-        j.sal.fs.removeDirTree(tmpdir)
-
-    def install(self, service):
+    if not j.do.checkSSHAgentAvailable():
         j.do._loadSSHAgent()
-        self.start(service=service)
 
-    def start(self, service):
+    if j.do.getSSHKeyPathFromAgent(name, die=False) is not None:
+        keyfile = j.do.getSSHKeyPathFromAgent(name)
+    elif service.hrd.get("key.path") != "":
+        keyfile = service.hrd.get("key.path")
+    else:
+        keyfile = j.sal.fs.joinPaths(tmpdir, name)
+        cmd = "ssh-keygen -t rsa -f %s -P '%s' " % (keyfile, service.hrd.getStr('key.passphrase'))
+        print(cmd)
+        j.sal.process.executeWithoutPipe(cmd)
+
+    if not j.sal.fs.exists(keyfile):
+        raise j.exceptions.Input("Cannot find ssh key location:%s" % keyfile)
+
+    keydest = j.sal.fs.joinPaths(service.path, "sshkey_%s" % service.instance)
+    if not keyfile == keydest:
+        service.hrd.set("key.path", keydest)
+        j.sal.fs.copyFile(keyfile, keydest)
+        j.sal.fs.copyFile(keyfile+".pub", keydest + ".pub")
+
+    privkey = j.sal.fs.fileGetContents(keydest)
+    pubkey = j.sal.fs.fileGetContents(keydest + ".pub")
+
+    service.hrd.set('key.pub', pubkey)
+    service.hrd.set('key.priv', privkey)
+
+    j.sal.fs.chmod(keydest, 0o600)
+    j.sal.fs.chmod(keydest+".pub", 0o600)
+
+    j.sal.fs.removeDirTree(tmpdir)
+
+
+def install(service):
+    from JumpScale import j
+
+    def start(service):
         """
         Add key to SSH Agent if not already loaded
         """
-        keypath = j.sal.fs.joinPaths(service.path, "sshkey_%s"%service.instance)
+        from JumpScale import j
+        keypath = j.sal.fs.joinPaths(service.path, "sshkey_%s" % service.instance)
         j.do.loadSSHKeys(keypath)
+    j.do._loadSSHAgent()
+    start(service=service)
 
 
-    # not sure we can remove, key can be used for something else
-    # def stop(self, service):
-    #     """
-    #     Remove key from SSH Agent
-    #     """
-    #     keyfile = self._getKeyPath()
-    #     if j.do.getSSHKeyPathFromAgent('$(key.name)', die=False) is not None:
-    #         keyloc = "/root/.ssh/%s" % '$(key.name)'
-    #         cmd = 'ssh-add -d %s' % keyfile
-    #         j.do.executeInteractive(cmd)
+def start(service):
+    """
+    Add key to SSH Agent if not already loaded
+    """
+    from JumpScale import j
+    keypath = j.sal.fs.joinPaths(service.path, "sshkey_%s" % service.instance)
+    j.do.loadSSHKeys(keypath)
