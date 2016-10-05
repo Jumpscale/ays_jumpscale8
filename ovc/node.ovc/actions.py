79d3640a4e8311a5d56804d576f89eb36ba9a9fc
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-10-05 10:06:38 +0400

    add node.ovc template

---
+++ ovc/node.ovc/actions.py
@@ -0,0 +1,58 @@
+def install(job):
+    service = job.service
+    vdc = service.parent
+
+    if 'g8client' not in vdc.producers:
+        raise j.exceptions.RuntimeError("no producer g8client found. cannot continue init of %s" % service)
+
+    g8client = vdc.producers["g8client"][0]
+    cl = j.clients.openvcloud.getFromService(g8client)
+    acc = cl.account_get(vdc.model.data.account)
+    space = acc.space_get(vdc.model.dbobj.name, vdc.model.data.location)
+
+    if service.name in space.machines:
+        # machine already exists
+        machine = space.machines[service.name]
+    else:
+        image_names = [i['name'] for i in space.images]
+        if service.model.data.osImage not in image_names:
+            raise j.exceptions.NotFound('Image %s not available for vdc %s' % (service.model.data.osImage, vdc.name))
+
+        datadisks = list(service.model.data.datadisks)
+        machine = space.machine_create(name=service.name,
+                                       image=service.model.data.osImage,
+                                       memsize=service.model.data.osSize,
+                                       disksize=service.model.data.bootdiskSize,
+                                       datadisks=datadisks)
+
+    service.model.data.machineId = machine.id
+    service.model.data.ipPublic = machine.space.model['publicipaddress']
+    service.model.data.ipPrivate = machine.get_machine_ip()[0]
+
+    for i, port in enumerate(service.model.data.ports):
+        ss = port.split(':')
+        if len(ss) == 2:
+            local_port, public_port = ss
+        else:
+            local_port, public_port = port, None
+
+        public, local = machine.create_portforwarding(publicport=public_port, localport=local_port, protocol='tcp')
+        service.model.data.ports[i] = "%s:%s" % (local, public)
+
+
+def uninstall(job):
+    service = job.service
+    vdc = service.parent
+
+    if 'g8client' not in vdc.producers:
+        raise j.exceptions.RuntimeError("no producer g8client found. cannot continue init of %s" % service)
+
+    g8client = vdc.producers["g8client"][0]
+    cl = j.clients.openvcloud.getFromService(g8client)
+    acc = cl.account_get(vdc.model.data.account)
+    space = acc.space_get(vdc.model.dbobj.name, vdc.model.data.location)
+
+    if service.name not in space.machines:
+        return
+    machine = space.machines[service.name]
+    machine.delete
