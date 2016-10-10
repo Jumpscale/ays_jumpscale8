7ad6e3637afba14da236ce6d7f7bf5bf18dea63a
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-10 10:02:51 +0200

    fix: Get space of only linked vdc

--- ovc/autosnapshotting/actions.py
+++ ovc/autosnapshotting/actions.py
@@ -5,12 +5,11 @@ def install(job):
     # Get space id from the parent (vdc)
     vdc = service.producers['vdc'][0]
 
-    # Get machine from spaces residing in g8client
+    # Get given space resides in g8client
     g8client = vdc.producers["g8client"][0]
     cl = j.clients.openvcloud.getFromService(g8client)
     cl = cl.account_get(vdc.model.data.account)
-    spaces = cl.spaces
+    space = cl.space_get(vdc.name, vdc.model.data.location)
 
-    for space in spaces:
-        for name, machine in space.machines.items():
-            machine.create_snapshot()
+    for name, machine in space.machines.items():
+        machine.create_snapshot()
