d2711fdd54b852df4da2062e596bea13b4f3b6d8
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-10 12:52:15 +0200

    WIP: Add recurring actions to autosnapshotting instance

--- ovc/autosnapshotting/actions.py
+++ ovc/autosnapshotting/actions.py
@@ -1,10 +1,48 @@
+def input(job):
+    args = job.model.args
+
+    # Validating startDate argument
+    if "startDate" not in args:
+        args["startDate"] = ""
+
+    # Validating endDate argument
+    if "endDate" not in args:
+        args["endDate"] = ""
+
+    # Add default value to snapshotInterval argument if empty or None
+    if "snapshotInterval" not in args or args["snapshotInterval"].strip() == "":
+        args["snapshotInterval"] = "2h"
+
+    # Add default value to retention argument if empty or None
+    if "retention" not in args or args["retention"].strip() == "":
+        args["retention"] = "5d"
+
+    return args
+
+
 def install(job):
-    """Take snapshot"""
+    """Configure recurring actions"""
+    RECURRING_ACTION_COUNT = 2
+    service.model.dbobj.init("recurringActions", RECURRING_ACTION_COUNT)
+
+    recurring = service.model.dbobj.recurringActions[0]
+    recurring.action = "snapshot"
+    recurring.period = j.data.types.duration.convertToSeconds(service.model.data.snapshotInterval)
+    recurring.log = True
+    recurring.lastRun = 0
+
+    recurring = service.model.dbobj.recurringActions[1]
+    recurring.action = "cleanup"
+    recurring.period = j.data.types.duration.convertToSeconds(service.model.data.retention)
+    recurring.log = True
+    recurring.lastRun = 0
+    service.saveAll()
+
+
+def snapshot(job):
     service = job.service
-
-    # Get space id from the parent (vdc)
-    vdc = service.producers['vdc'][0]
-
+    # Get spaces id from the parent (vdc)
+    vdc = service.producers["vdc"][0]
     # Get given space resides in g8client
     g8client = vdc.producers["g8client"][0]
     cl = j.clients.openvcloud.getFromService(g8client)
