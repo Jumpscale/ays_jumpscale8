d4cbae0c7bef47a2d18bddd2e803d78ab76a12ad
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-11 10:27:26 +0200

    Finishing implementing autsnapshotting

--- ovc/autosnapshotting/actions.py
+++ ovc/autosnapshotting/actions.py
@@ -21,9 +21,10 @@ def input(job):
 
 
 def install(job):
+    """Configure recurring actions"""
+
     service = job.service
 
-    """Configure recurring actions"""
     RECURRING_ACTION_COUNT = 2
     service.model.dbobj.init("recurringActions", RECURRING_ACTION_COUNT)
 
@@ -35,17 +36,25 @@ def install(job):
 
     recurring = service.model.dbobj.recurringActions[1]
     recurring.action = "cleanup"
-    recurring.period = j.data.types.duration.convertToSeconds("30s")
+    recurring.period = j.data.types.duration.convertToSeconds("1d")
     recurring.log = True
     recurring.lastRun = 0
     service.saveAll()
 
 
 def snapshot(job):
+    """Taking snapshot of all machines in the given space if it meets the conditions specified in the schema"""
     from dateutil import parser
     import datetime
 
-    def create_snapshot(service):
+    service = job.service
+    START_DATE = parser.parse(service.model.data.startDate) if service.model.data.endDate != "" else ""
+    END_DATE = parser.parse(service.model.data.endDate) if service.model.data.endDate != "" else ""
+
+    start_date_valid = START_DATE == "" or START_DATE < datetime.datetime.now()
+    end_date_valid = END_DATE == "" or END_DATE > datetime.datetime.now()
+
+    if start_date_valid and end_date_valid:
         # Get spaces id from the parent (vdc)
         vdc = service.producers["vdc"][0]
         # Get given space resides in g8client
@@ -56,28 +65,13 @@ def snapshot(job):
         for name, machine in space.machines.items():
             machine.create_snapshot()
 
-    def date_input(date):
-        if date != "":
-            parser.parse(service.model.data.startDate)
-        return date
-
-    service = job.service
-    START_DATE = date_input(service.model.data.startDate)
-    END_DATE = date_input(service.model.data.endDate)
-
-    start_date_valid = START_DATE == "" or START_DATE < datetime.datetime.now()
-    end_date_valid = END_DATE == "" or END_DATE > datetime.datetime.now()
-    if start_date_valid and end_date_valid:
-            create_snapshot(service)
-
 
 def cleanup(job):
-    from dateutil import parser
-    import datetime
-    import re
+    """Removing snapshot that's lifetime exceeds the given retention"""
+    import time
 
     service = job.service
-    RETENTION = int(re.findall('\d+', service.model.data.retention)[0])
+    RETENTION = j.data.types.duration.convertToSeconds(service.model.data.retention)
 
     # Get spaces id from the parent (vdc)
     vdc = service.producers["vdc"][0]
@@ -89,7 +83,8 @@ def cleanup(job):
 
     for name, machine in space.machines.items():
         for snapshot in machine.list_snapshots():
-            delta = datetime.datetime.now() - parser.parse(snapshot)
+            # Get the delta time in seconds
+            delta = int(time.time()) - snapshot['epoch']
             if delta > RETENTION:
-                machine.deleteSnapshot(snapshot['name'], snapshot['epoch'])
+                machine.delete_snapshot(snapshot['epoch'])
 
