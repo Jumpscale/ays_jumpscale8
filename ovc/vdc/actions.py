db873b26963cb2b4357fbde417b467eae1182eb2
Khaled Karam <mohamedk@greenitglobe.com>
2016-10-13 15:19:04 +0200

    Support processChange for multiple category changes

--- ovc/vdc/actions.py
+++ ovc/vdc/actions.py
@@ -40,35 +40,37 @@ def processChange(job):
     service = job.service
 
     args = job.model.args
-    for key, value in args.items():
-        if key != "changeCategory":
-            setattr(service.model.data, key, value)
 
-    if 'g8client' not in service.producers:
-        raise j.exceptions.AYSNotFound("no producer g8client found. cannot continue init of %s" % service)
+    if args["changeCategory"] == "dataschema":
+        for key, value in args.items():
+            if key != "changeCategory":
+                setattr(service.model.data, key, value)
 
-    g8client = service.producers["g8client"][0]
-    cl = j.clients.openvcloud.getFromService(g8client)
-    acc = cl.account_get(service.model.data.account)
-    # Get given space, raise error if not found
-    space = acc.space_get(name=service.model.dbobj.name,
-                          location=service.model.data.location,
-                          create=False)
+        if 'g8client' not in service.producers:
+            raise j.exceptions.AYSNotFound("no producer g8client found. cannot continue init of %s" % service)
 
-    authorized_users = space.authorized_users
-    users = service.model.data.vdcUsers  # Users to be authorized_users
+        g8client = service.producers["g8client"][0]
+        cl = j.clients.openvcloud.getFromService(g8client)
+        acc = cl.account_get(service.model.data.account)
+        # Get given space, raise error if not found
+        space = acc.space_get(name=service.model.dbobj.name,
+                            location=service.model.data.location,
+                            create=False)
 
-    # Authorize users
-    for user in users:
-        if user not in authorized_users:
-            space.authorize_user(username=user)
+        authorized_users = space.authorized_users
+        users = service.model.data.vdcUsers  # Users to be authorized_users
 
-    # Unauthorize users not in the schema
-    for user in authorized_users:
-        if user not in users:
-            space.unauthorize_user(username=user)
+        # Authorize users
+        for user in users:
+            if user not in authorized_users:
+                space.authorize_user(username=user)
 
-    service.save()
+        # Unauthorize users not in the schema
+        for user in authorized_users:
+            if user not in users:
+                space.unauthorize_user(username=user)
+
+        service.save()
 
 
 def uninstall(job):
