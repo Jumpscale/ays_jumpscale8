f448df103e259fbb2d028e1f47d87996852aa95b
christophe de Carvalho Pereira Martincs <christophe.dcpm@gmail.com>
2016-10-13 14:35:21 +0400

    sshkey: support creation from existing keys

--- clients/sshkey/actions.py
+++ clients/sshkey/actions.py
@@ -7,18 +7,20 @@ def input(job):
     """
 
     # THIS ONE IS FIXED
-
     args = {}
 
-    if 'key.path' in job.model.args:
+    if 'key.path' in job.model.args and job.model.args['key.path'] is not None and job.model.args['key.path'] != '':
         path = job.model.args['key.path']
         if not j.sal.fs.exists(path, followlinks=True):
             raise j.exceptions.Input(message="Cannot find ssh key:%s for service:%s" %
                                      (path, job.service), level=1, source="", tags="", msgpub="")
 
+        args['key.path'] = j.sal.fs.joinPaths(job.service.path, "id_rsa")
+        j.sal.fs.createDir(job.service.path)
+        j.sal.fs.copyFile(path, args['key.path'])
+        j.sal.fs.copyFile(path + '.pub', args['key.path'] + '.pub')
         args["key.priv"] = j.sal.fs.fileGetContents(path)
         args["key.pub"] = j.sal.fs.fileGetContents(path + '.pub')
-        args.pop('key.path')
 
     if 'key.name' in job.model.args:
         path = j.do.getSSHKeyPathFromAgent("kds")
@@ -32,12 +34,12 @@ def input(job):
 
     if 'key.priv' not in args or args['key.priv'].strip() == "":
         print("lets generate private key")
-        path = j.sal.fs.joinPaths(job.service.path, "id_rsa")
+        args['key.path'] = j.sal.fs.joinPaths(job.service.path, "id_rsa")
         j.sal.fs.createDir(job.service.path)
-        j.sal.fs.remove(path)
-        cmd = "ssh-keygen -q -t rsa -f %s -N ''" % (path)
+        j.sal.fs.remove(args['key.path'])
+        cmd = "ssh-keygen -q -t rsa -f %s -N ''" % (args['key.path'])
         rc, out = j.sal.process.execute(cmd, die=True, outputToStdout=False, ignoreErrorOutput=False)
-        args["key.priv"] = j.sal.fs.fileGetContents(path)
-        args["key.pub"] = j.sal.fs.fileGetContents(path + '.pub')
+        args["key.priv"] = j.sal.fs.fileGetContents(args['key.path'])
+        args["key.pub"] = j.sal.fs.fileGetContents(args['key.path'] + '.pub')
 
     return args
