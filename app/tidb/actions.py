83ebca81ce0ae075597acbc2c2c9caca3b6db07a
Ahmed T. Youssef <xmonader@gmail.com>
2016-10-16 17:53:52 +0200

    init tidb, owncloud services

--- app/tidb/actions.py
+++ app/tidb/actions.py
@@ -10,20 +10,6 @@ def install(job):
     # dbuser = service.model.data.dbuser
     # dbpassword = service.model.data.dbpass
 
-    cuisine.apps.tidb.start_pd_server()
-    cuisine.apps.tidb.start_tikv()
-    import time;
-    time.sleep(5)
-    cuisine.apps.tidb.start_tidb()
-    cuisine.package.ensure('mysql-client')
-
-    # import time
-    # time.sleep(5)
-    #
-    # with cuisine.apps.tidb.dbman() as m:
-    #     try:
-    #         m.create_database(database=dbname)
-    #         m.create_dbuser(host="127.0.0.1", username=dbuser, passwd=dbpassword)
-    #     except:
-    #         pass  # user created already.
-    #     m.grant_user(host="127.0.0.1", username=dbuser, database=dbname)
+    cuisine.apps.tidb.start()
+    C = "apt-get update && apt-get install mysql-client-core-5.7 -y"
+    cuisine.core.execute_bash(C)
