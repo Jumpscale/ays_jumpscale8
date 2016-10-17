b41be40ff1ecb5352f977105989710db21bcf4d5
Ahmed T. Youssef <xmonader@gmail.com>
2016-10-17 15:47:01 +0200

    working owncloud servicetemplate

--- app/owncloud/actions.py
+++ app/owncloud/actions.py
@@ -7,8 +7,16 @@ def install(job):
 
     cuisine.apps.owncloud.install(start=False)
 
+    tidb = service.producers['tidb'][0]
+    tidbos = tidb.parent
+    tidbdocker = tidbos.parent
+    tidbhost = tidbdocker.model.data.ipPrivate
 
-def start(job):
-    service = job.service
-    cuisine = service.executor.cuisine
-    cuisine.apps.owncloud.start(service.data.model.sitename)
+    tidbuser = service.model.data.tidbuser
+    tidbpassword = service.model.data.tidbpassword
+    # dbhost=tidb.model.data.dbhost
+    # dbuser=tidb.model.data.dbuser
+    # dbpass=tidb.model.data.dbpass
+
+    #cuisine.apps.owncloud.install(start=False)
+    cuisine.apps.owncloud.start(sitename=sitename, dbhost=tidbhost, dbuser=tidbuser, dbpass=tidbpassword)
