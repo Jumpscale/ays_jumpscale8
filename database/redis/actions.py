5417bf2e9f973e7e5379d26dc94155faa9cebe91
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-10-17 13:32:15 +0400

    fix creation of redis condfig

--- database/redis/actions.py
+++ database/redis/actions.py
@@ -5,8 +5,9 @@ def install(job):
     cuisine.apps.redis.install()
     cuisine.apps.redis.start(
         name=service.name,
-        ip=service.model.data.host,
+        ip=service.model.data.host if service.model.data.host != '' else None,
         port=service.model.data.port,
+        unixsocket=service.model.data.unixsocket if service.model.data.unixsocket != '' else None,
         maxram=service.model.data.maxram,
         appendonly=service.model.data.appendonly)
 
@@ -18,11 +19,14 @@ def start(job):
     cuisine.apps.redis.install()
     cuisine.apps.redis.start(
         name=service.name,
-        ip=service.model.data.host,
+        ip=service.model.data.host if service.model.data.host != '' else None,
         port=service.model.data.port,
+        unixsocket=service.model.data.unixsocket if service.model.data.unixsocket != '' else None,
         maxram=service.model.data.maxram,
         appendonly=service.model.data.appendonly)
 
 
 def stop(job):
+    service = job.service
+    cuisine = service.executor.cuisine
     cuisine.apps.redis.stop(job.service.name)
