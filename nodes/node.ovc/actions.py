f17393b9eef4c6dab54b5cd969afada8130c00e7
Ahmed T. Youssef <xmonader@gmail.com>
2016-10-23 12:00:49 +0300

    Tempdiskovc (#65)
    * init support of executor in ovc node and disks schema
    
    * remove ipdb, set disk_limit
    
    * using json serializer and remove try except block

--- nodes/node.ovc/actions.py
+++ nodes/node.ovc/actions.py
@@ -23,15 +23,6 @@ def install(job):
                                        memsize=service.model.data.memory,
                                        disksize=service.model.data.bootdiskSize)
 
-    # Add disks to machine
-    for data_disk in service.producers.get('disk', []):
-        disk_args = data_disk.model.data
-        disk_id = machine.add_disk(name=data_disk.model.dbobj.name,
-                                   description=disk_args.description,
-                                   size=disk_args.size,
-                                   type=disk_args.type.upper())
-        machine.disk_limit_io(disk_id, disk_args.maxIOPS)
-
     service.model.data.machineId = machine.id
     service.model.data.ipPublic = machine.space.model['publicipaddress']
     ip, vm_info = machine.get_machine_ip()
@@ -49,6 +40,63 @@ def install(job):
 
         public, local = machine.create_portforwarding(publicport=public_port, localport=local_port, protocol='tcp')
         service.model.data.ports[i] = "%s:%s" % (public, local)
+    if 'sshkey' not in service.producers:
+        raise j.exceptions.AYSNotFound("No sshkey service consumed. please consume an sshkey service")
+
+    sshkey = service.producers['sshkey'][0]
+    service.logger.info("authorize ssh key to machine")
+    node = service
+
+    # Looking in the parents chain is needed when we have nested nodes (like a docker node on top of an ovc node)
+    # we need to find all the ports forwarding chain to reach the inner most node.
+    ssh_port = '22'
+
+    for port in service.model.data.ports:
+        src, _, dst = port.partition(':')
+        if ssh_port == dst:
+            ssh_port = src
+            break
+
+    service.model.data.sshPort = int(ssh_port)
+
+    sshkey = service.producers['sshkey'][0]
+    key_path = j.sal.fs.joinPaths(sshkey.path, 'id_rsa')
+    password = node.model.data.sshPassword if node.model.data.sshPassword != '' else None
+
+    # used the login/password information from the node to first connect to the node and then authorize the sshkey for root
+    executor = j.tools.executor.getSSHBased(addr=node.model.data.ipPublic, port=service.model.data.sshPort,
+                                            login=node.model.data.sshLogin, passwd=password,
+                                            allow_agent=True, look_for_keys=True, timeout=5, usecache=False,
+                                            passphrase=None, key_filename=key_path)
+    executor.cuisine.ssh.authorize("root", sshkey.model.data.keyPub)
+
+    cuisine = executor.cuisine
+
+    devicesmap = {}  # diskname -> devicename
+    seendevices = []  # only the ones with mountpoint is null
+    # Add disks to machine
+    for data_disk in service.producers.get('disk', []):
+        disk_args = data_disk.model.data
+        disk_id = machine.add_disk(name=data_disk.model.dbobj.name,
+                                   description=disk_args.description,
+                                   size=disk_args.size,
+                                   type=disk_args.type.upper())
+        rc, out, err = cuisine.core.run("lsblk -J", die=False)
+
+        jsonout = j.data.serializers.json.loads(out)
+        devices = [x for x in jsonout['blockdevices'] if x['mountpoint'] is None and x['type'] == 'disk'] # should be only 1
+
+        for dv in devices:
+            if 'children' in dv:
+                continue
+            if dv['name'] not in seendevices:
+                data_disk.model.data.devicename = dv['name']
+                data_disk.saveAll()
+                devicesmap[data_disk.model.dbobj.name] = dv['name']
+                seendevices.append(dv['name'])
+
+
+        machine.disk_limit_io(disk_id, disk_args.maxIOPS)  #COMMENTED BECAUSE OF GIG
 
     service.saveAll()
 
