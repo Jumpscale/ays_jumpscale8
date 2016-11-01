88800621c268453dc4aa4bcc90c24810e6d87028
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-01 14:32:54 +0400

    configure metadata during ayscockpit install + fix start/stop method

--- jumpscale/ayscockpit/actions.py
+++ jumpscale/ayscockpit/actions.py
@@ -5,21 +5,22 @@ def install(job):
     if 'redis' not in service.producers:
         raise j.exceptions.AYSNotFound("Can't find redis service in producers.")
 
+    # configure redis connection for AYS
     redis = service.producers['redis'][0]
-    daemon_cfg = {
-        'redis': {}
-    }
+    # this line create the default config if it doesn't exsits yet
+    config = j.atyourservice.config
+
     if redis.model.data.unixsocket != '':
-        daemon_cfg['redis']['unixsocket'] = redis.model.data.unixsocket
+        config['redis']['unixsocket'] = redis.model.data.unixsocket
     else:
-        daemon_cfg['redis'] = {
+        config['redis'] = {
             'host': redis.model.data.host,
             'port': redis.model.data.port,
         }
 
     cfg_path = cuisine.core.args_replace('$cfgDir/ays/ays.conf')
     cuisine.core.dir_ensure(j.sal.fs.getParent(cfg_path))
-    cuisine.core.file_write(cfg_path, j.data.serializer.toml.dumps(daemon_cfg))
+    cuisine.core.file_write(cfg_path, j.data.serializer.toml.dumps(config))
 
     # configure AYS daemon
     cmd = 'ays start'
@@ -32,7 +33,7 @@ def install(job):
     cuisine.core.file_write('$appDir/ays_api/ays_api/apidocs/api.raml', raml)
 
     cmd = 'jspython api_server'
-    pm.ensure(cmd=cmd, name='cockpit_api_%s' % service.name, path='$appDir/ays_api')
+    pm.ensure(cmd=cmd, name='cockpit_api_%s' % service.name, path=cuisine.core.args_replace('$appDir/ays_api'))
 
 
 def start(job):
@@ -40,11 +41,17 @@ def start(job):
     cuisine = service.executor.cuisine
 
     pm = cuisine.processmanager.get('tmux')
-    pm.start(name='cockpit_%s' % service.name)
+
+    cmd = 'ays start'
+    pm.ensure(cmd=cmd, name='cockpit_daemon_%s' % service.name, path=cuisine.core.args_replace('$cfgDir/ays'))
+
+    cmd = 'jspython api_server'
+    pm.ensure(cmd=cmd, name='cockpit_api_%s' % service.name, path=cuisine.core.args_replace('$appDir/ays_api'))
 
 
 def stop(job):
     service = job.service
     cuisine = service.executor.cuisine
     pm = cuisine.processmanager.get('tmux')
-    pm.stop(name='cockpit_%s' % service.name)
+    pm.stop(name='cockpit_api_%s' % service.name)
+    pm.stop(name='cockpit_daemon_%s' % service.name)
