0684e7b380fe9a5f18e0aab030c64068c5b4b792
Reem Khamis <khamisr@codescalers.com>
2016-11-02 16:39:45 +0200

    Fix undefinied variable usage

--- clients/sshkey/actions.py
+++ clients/sshkey/actions.py
@@ -42,8 +42,6 @@ def input(job):
         print("lets generate private key")
         args['key.path'] = j.sal.fs.joinPaths(job.service.path, "id_rsa")
         j.sal.fs.createDir(job.service.path)
-        j.sal.fs.copyFile(path, args['key.path'])
-        j.sal.fs.copyFile(path + '.pub', args['key.path'] + '.pub')
         j.sal.fs.remove(args['key.path'])
         cmd = "ssh-keygen -q -t rsa -f %s -N ''" % (args['key.path'])
         rc, out = j.sal.process.execute(cmd, die=True, outputToStdout=False, ignoreErrorOutput=False)
