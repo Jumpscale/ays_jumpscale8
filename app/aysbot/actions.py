787848ee79d7d7f9cf8cc1cd1da52bd3eddaf0ba
abdulrahmantkhalifa <abdulrahman.t.khalifa@gmail.com>
2016-11-21 15:39:44 +0200

    WIP: adjusted cockpit service to install ays_bot
    not tested yet

--- app/aysbot/actions.py
+++ app/aysbot/actions.py
@@ -1,11 +1,7 @@
 def install(job):
     service = job.service
     cuisine = job.service.executor.cuisine
-    base_bot_path = "$codeDir/github/jumpscale/jscockpit/ays_bot"
-    if not cuisine.core.dir_exists(base_bot_path):
-        cuisine.development.git.pullRepo('https://github.com/Jumpscale/jscockpit', ssh=False)
 
-    cuisine.core.file_copy(base_bot_path, "$appDir", recursive=True)
     cfg = {'token': service.model.data.botToken,
            'oauth': {
                'host': service.model.data.oauthHost,
@@ -17,11 +13,14 @@ def install(job):
                'itsyouonlinehost': service.model.data.oauthItsyouonlinehost
                }
            }
-    config = j.data.serializer.toml.dumps(cfg)
+    cuisine.apps.aysbot.configure(cfg=cfg)
+    cuisine.apps.aysbot.install()
 
-    cuisine.core.dir_ensure("$cfgDir/ays_bot")
-    cuisine.core.file_write("$cfgDir/ays_bot/config.toml", config)
+def start(job):
+    cuisine = job.service.executor.cuisine
+    cuisine.processmanager.start('ays_bot')
 
-    cuisine.processmanager.ensure(name="ays_bot",
-                                  cmd="jspython ays-bot -c $cfgDir/ays_bot/config.toml",
-                                  path="$appDir/ays_bot")
+
+def stop(job):
+    cuisine = job.service.executor.cuisine
+    cuisine.processmanager.stop('ays_bot')
