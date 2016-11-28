Jumpscale/ays_jumpscale8

commit d7e44e64ed736679a43f734a5bd84dc06bf33c68

WIP: add ays services for kvm

disk/disk.qcow2/actions.py
@@ -1,0 +1,16 @@
+from JumpScale import j
+
+def install(job):
+	job.model().create()
+
+def model(job):
+    service = job.service
+    c = service.executor.cuisine
+    controller = c.systemservices.kvm._controller
+    vmpool = service.producers['vmpool'][0]
+    name = service.model.dbobj.name
+    size = service.model.data.size
+    backingStore = service.producers.get('backingStore', [None])[0]
+
+    pool = j.sal.kvm.Pool(controller, vmpool.model.data.name)
+    return j.sal.kvm.Disk(controller, pool, name, size, backingStore.path() if backingStore else None)

clients/openvswitch/actions.py
@@ -1,0 +1,53 @@
+from JumpScale import j
+
+def install(job):
+
+    service = job.service
+    c = service.executor.cuisine
+
+    c.systemservices.openvswitch.install()
+
+    name = service.model.data.name
+    ip = service.model.data.ip
+    netmask = service.model.data.netmask
+    masquerading = service.model.data.masquerading
+    ipconfig = service.model.data.ipconfig
+    dhcp = service.model.data.dhcp
+
+    # create bridge vms1
+    c.systemservices.openvswitch.networkCreate(name, ovs=False, start=False)
+    if ipconfig:
+        # configure the network and the natting
+        c.net.netconfig(name, ip, netmask, masquerading=masquerading)
+        c.processmanager.start('systemd-networkd')
+    if dhcp:
+        # add a dhcp sercer to the bridge
+        c.apps.dnsmasq.config(name)
+        c.apps.dnsmasq.install()
+
+def destroy(job):
+
+    service = job.service
+    c = service.executor.cuisine
+
+    name = service.model.data.name
+
+    j.sal.kvm.Network.get_by_name(c.systemservices.openvswitch._controller, name).destroy()
+
+def start(job):
+
+    service = job.service
+    c = service.executor.cuisine
+
+    name = service.model.data.name
+
+    j.sal.kvm.Network.get_by_name(c.systemservices.openvswitch._controller, name).start()
+
+def stop(job):
+
+    service = job.service
+    c = service.executor.cuisine
+
+    name = service.model.data.name
+
+    j.sal.kvm.Network.get_by_name(c.systemservices.openvswitch._controller, name).stop()

clients/vmpool/actions.py
@@ -1,0 +1,11 @@
+from JumpScale import j
+
+def install(job):
+
+    service = job.service
+    c = service.executor.cuisine
+
+    name = service.model.data.name
+
+    c.systemservices.kvm.install()
+    c.systemservices.kvm.poolCreate(name)

disk/disk.url/actions.py
@@ -1,0 +1,20 @@
+from JumpScale import j
+
+
+def install(job):
+
+    service = job.service
+    c = service.executor.cuisine
+    url = service.model.dbobj.url
+
+    c.systemservices.kvm.download_image(url)
+
+
+def path(job):
+    service = job.service
+    c = service.executor.cuisine
+    controller = c.systemservices.kvm._controller
+    url = service.model.dbobj.url
+    name = url.split('/')[-1]
+
+    return j.sal.fs.joinPaths(controller, 'images', name)

disk/node.vm/actions.py
@@ -1,0 +1,20 @@
+from JumpScale import j
+
+controller, name, new_disks, new_nics, memory, cpucount)
+
+def install(job):
+    service = job.service
+    c = service.executor.cuisine
+    controller = c.systemservices.kvm._controller
+    name = service.model.dbobj.name
+    disks = [disk.model() for disk in service.producers['disks']]
+
+    nics = [disk.model.dbobj.name for disk in service.producers['bridges']]
+
+    new_nics = list(map(lambda x: j.sal.kvm.Interface(controller, x,
+                                                          j.sal.kvm.Network(controller, x, x, [])), nics))
+    memory = service.model.dbobj.memory
+    cpucount = service.model.dbobj.cpucount
+    cloud_init = service.model.dbobj.cloud_init
+
+    machine = j.sal.kvm.Machine(controller, name, disks, new_nics, memory, cpucount, cloud_init=cloud_init)

nodes/node.kvm/actions.py
@@ -1,0 +1,175 @@
+def input(job):
+    r = job.service.aysrepo
+
+    if "node" in job.model.args:
+        res = job.model.args
+        res["os"] = res["node"]
+        res.pop("node")
+        job.model.args = res
+
+    return job.model.args
+
+
+def init(job):
+
+    r = job.service.aysrepo
+
+    a = r.actorGet("os.ssh.ubuntu")
+    if r.serviceGet("os", job.service.name, die=False) == None:
+        s = a.serviceCreate(instance=job.service.name, args={
+                            "node": job.service.name, "sshkey": job.service.model.data.sshkey})
+
+
+def install(job):
+    service = job.service
+    # create the docker container based on the data
+
+    """
+    version: '2'
+    services:
+      mongo:
+        container_name: mongodb
+        image: mongo
+        command: "--replSet c0"
+        network_mode: "bridge"
+        labels:
+          SERVICE_TAGS: traefik.enable=false
+        ports:
+          - "27017"
+        volumes:
+          - /var/mongo/:/data/
+    """
+    compose = {
+        'version': '2',
+        'services': {
+            service.name: {
+                'container_name': service.name,
+                'image': service.model.data.image,
+                'command': service.model.data.cmd,
+                'network_mode': 'bridge',
+                'ports': list(service.model.data.ports),
+                'volumes': list(service.model.data.volumes)
+            }
+        }
+    }
+
+    cuisine = service.executor.cuisine
+
+    base = j.sal.fs.joinPaths('/var', 'dockers', service.name)
+    cuisine.core.dir_ensure(base)
+    cuisine.core.file_write(
+        j.sal.fs.joinPaths(base, 'docker-compose.yml'),
+        j.data.serializer.yaml.dumps(compose)
+    )
+
+    code, _, err = cuisine.core.run('cd {} && docker-compose up -d'.format(base))
+    if code != 0:
+        raise RuntimeError('failed to provision docker container: %s' % err)
+
+    code, docker_id, err = cuisine.core.run('cd {} && docker-compose ps -q'.format(base))
+    if code != 0:
+        raise RuntimeError('failed to get the container id: %s' % err)
+    service.model.data.id = docker_id
+
+    # get the ipaddress and ports
+    code, inspected, err = cuisine.core.run('docker inspect {id}'.format(id=docker_id), showout=False)
+    if code != 0:
+        raise RuntimeError('failed to inspect docker %s: %s' % (service.name, err))
+
+    inspected = j.data.serializer.json.loads(inspected)
+    info = inspected[0]
+    ipaddress = info['NetworkSettings']['IPAddress']
+    ports = info['NetworkSettings']['Ports']
+
+    # find parent node.
+    node = None
+    for parent in service.parents:
+        if parent.model.role == 'node':
+            node = parent
+
+    if node is None:
+        raise RuntimeError('cannot find parent node')
+
+    docker_ports = []
+
+    for dst_port_spec, host_port_info in ports.items():
+        dst_port, _, dst_proto = dst_port_spec.partition('/')
+        if host_port_info is None:
+            # no bindings
+            continue
+        host_port = host_port_info[0]['HostPort']
+        docker_ports.append('{src}:{dst}'.format(src=host_port, dst=dst_port))
+
+    service.model.data.ipPrivate = ipaddress
+    service.model.data.ipPublic = node.model.data.ipPublic
+    service.model.data.sshLogin = 'root'
+    service.model.data.sshPassword = 'gig1234'
+
+    service.model.data.ports = docker_ports
+
+    service.saveAll()
+
+
+def start(job):
+    service = job.service
+    cuisine = service.executor.cuisine
+
+    docker_id = service.model.data.id
+    if docker_id is None or docker_id == '':
+        raise j.exceptions.RuntimeError('docker id is not known')
+
+    cuisine.core.run('docker start {id}'.format(id=docker_id))
+
+     # get the ipaddress and ports
+    code, inspected, err = cuisine.core.run('docker inspect {id}'.format(id=docker_id), showout=False)
+    if code != 0:
+        raise RuntimeError('failed to inspect docker %s: %s' % (service.name, err))
+
+    inspected = j.data.serializer.json.loads(inspected)
+    info = inspected[0]
+    ipaddress = info['NetworkSettings']['IPAddress']
+    ports = info['NetworkSettings']['Ports']
+
+    docker_ports = []
+    for dst_port_spec, host_port_info in ports.items():
+        dst_port, _, dst_proto = dst_port_spec.partition('/')
+        if host_port_info is None:
+            # no bindings
+            continue
+        host_port = host_port_info[0]['HostPort']
+        docker_ports.append('{src}:{dst}'.format(src=host_port, dst=dst_port))
+
+    service.model.data.ipPrivate = ipaddress
+    service.model.data.ports = docker_ports
+
+    service.saveAll()
+
+
+def stop(job):
+    service = job.service
+    cuisine = service.executor.cuisine
+
+    docker_id = service.model.data.id
+    if docker_id is None or docker_id == '':
+        raise j.exceptions.RuntimeError('docker id is not known')
+
+    cuisine.core.run('docker stop {id}'.format(id=docker_id))
+
+
+def uninstall(job):
+    service = job.service
+    cuisine = service.executor.cuisine
+
+    docker_id = service.model.data.id
+    if docker_id is None or docker_id == '':
+        raise j.exceptions.RuntimeError('docker id is not known')
+
+    cuisine.core.run('docker rm -f {id}'.format(id=docker_id))
+
+    service.model.data.id = ''
+    service.model.data.ipPrivate = ''
+    service.model.data.ipPublic = ''
+    service.model.data.sshLogin = 'root'
+    service.model.data.sshPassword = 'gig1234'
+
+    service.saveAll()

clients/openvswitch/schema.hrd
@@ -1,0 +1,6 @@
+name = type:str
+ip = type:str default:'10.0.4.1'
+netmask = type:int default:24
+masquerading = type:bool default:True
+ipconfig = type:bool default:True
+dhcp = type:bool default:True

clients/vmpool/schema.hrd
@@ -1,0 +1,1 @@
+name = type:str

commit e03441a25ed7030f1d03e020f981d3964c8737a1

Fix indentation error introduced in d7e44e6

disk/disk.qcow2/actions.py
@@ -1,7 +1,9 @@
 from JumpScale import j
 
+
 def install(job):
-	job.model().create()
+    job.model().create()
+
 
 def model(job):
     service = job.service