3412457369ba3bdebaf988ffbde4406af514ddad
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2016-11-29 11:20:15 +0400

    start all required service for libvirt to work

--- app/kvm/actions.py
+++ app/kvm/actions.py
@@ -11,22 +11,28 @@ def install(job):
 
 def start(job):
     cuisine = job.service.executor.cuisine
-    if not cuisine.processmanager.exists('libvirt-bin'):
-        raise j.exceptions.RuntimeError("libvirt-bin service doesn't exists. \
-                                         it should have been created during installation of this service")
 
-    cuisine.processmanager.start('libvirt-bin')
+    services_to_start = ['libvirt-bin', 'virtlogd']
+    for service in services_to_start:
+        if not cuisine.processmanager.exists(service):
+            raise j.exceptions.RuntimeError("{} service doesn't exists. \
+                                             it should have been created during installation of this service".format(service))
+
+        cuisine.processmanager.start(service)
 
     job.service.model.actions['stop'].state = 'new'
     job.service.saveAll()
 
 def stop(job):
     cuisine = job.service.executor.cuisine
-    if not cuisine.processmanager.exists('libvirt-bin'):
-        raise j.exceptions.RuntimeError("libvirt-bin service doesn't exists. \
-                                         it should have been created during installation of this service")
 
-    cuisine.processmanager.stop('libvirt-bin')
+    services_to_start = ['libvirt-bin', 'virtlogd']
+    for service in services_to_start:
+        if not cuisine.processmanager.exists(service):
+            raise j.exceptions.RuntimeError("{} service doesn't exists. \
+                                             it should have been created during installation of this service".format(service))
+
+        cuisine.processmanager.stop(service)
 
     job.service.model.actions['start'].state = 'new'
     job.service.saveAll()
