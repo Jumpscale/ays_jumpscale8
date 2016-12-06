b6a09eed0daece52171d47c3f5030d2da969ee19
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-06 22:13:35 +0200

    Add jwt param to g8client

--- templates/clients/g8client/actions.py
+++ templates/clients/g8client/actions.py
@@ -6,4 +6,10 @@ def input(job):
     if "account" not in args or args["account"].strip() == "":
         args['account'] = args["login"]
 
+    login = args.get('login', '')
+    password = args.get('password', '')
+    jwt = args.get('jwt', '')
+    if login == '' and password == '' and jwt == '':
+        raise j.exceptions.Input("Either username/password or jwt should be entered for %s" % job.service)
+
     return args
