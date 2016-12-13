93949ada02f0c63fbf32d49d151cf0cd31b1aca3
Reem Khamis <rkhamis@users.noreply.github.com>
2016-12-13 18:42:48 +0200

    Avoid apt lock (#125)

--- templates/app/app_docker/actions.py
+++ templates/app/app_docker/actions.py
@@ -5,10 +5,20 @@ def install(job):
     '''
     make sure docker is properly installed (use cuisine functionality)
     '''
+    import time
     service = job.service
     cuisine = service.executor.cuisine
     if not cuisine.systemservices.docker.isInstalled():
-        cuisine.systemservices.docker.install()
+        timeout = 500
+        while timeout > 0:
+            try:
+                cuisine.systemservices.docker.install()
+            except Exception as e:
+                if 'Resource temporarily unavailable' not in str(e):
+                    raise RuntimeError(e)
+                time.sleep(5)
+                timeout -= 500
+
 
     if not cuisine.systemservices.dockercompose.isInstalled():
         cuisine.systemservices.dockercompose.install()
