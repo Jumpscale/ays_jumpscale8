9bb617696c7e3906d2ce8d18f51bddd8971ac2b4
Khaled K. Badr <khaledkbadr@outlook.com>
2017-03-02 13:13:47 +0200

    remove rogerthat services

--- templates/app/appscale/actions.py
+++
@@ -1,9 +0,0 @@
-def install(job):
-    """
-    Installing appscale
-    """
-    service = job.service
-    cuisine = service.executor.cuisine
-
-    appscale_tag = service.model.data.appscaletag
-    cuisine.apps.appscale.build(tag=appscale_tag)
--- templates/app/rogerthatmfr/actions.py
+++
@@ -1,8 +0,0 @@
-def install(job):
-    """
-    Installing mobidick
-    """
-    service = job.service
-    cuisine = service.executor.cuisine
-    build_path = cuisine.apps.rogerthatmfr.build()
-    cuisine.core.run("cd /root && appscale deploy %s" % build_path, die=True)
