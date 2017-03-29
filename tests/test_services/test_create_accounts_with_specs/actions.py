c2dde67fef98f73d377af0131a6aee386a97a14b
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2017-03-29 16:39:30 +0200

    edit task 591

--- tests/test_services/test_create_accounts_with_specs/actions.py
+++ tests/test_services/test_create_accounts_with_specs/actions.py
@@ -29,14 +29,17 @@ def test(job):
         session.post(url=login_url, data=credential)
 
         accounts = service.producers['account']
-        accountIds =[x.model.data.accountID for x in accounts]
-
         API_URL = url + '/restmachine/cloudapi/accounts/get'
 
-        for accountId in accountIds:
+        for account  in accounts:
+            accountId=account.model.data.accountID
+            account_specs=[account.model.data.maxMemoryCapacity,account.model.data.maxDiskCapacity,
+                           account.model.data.maxNumPublicIP,account.model.data.maxCPUCapacity]
             API_BODY = {'accountId': accountId}
             response = session.post(url=API_URL, data=API_BODY)
-            if response.status_code != 200:
+            specs= response.json()['resourceLimits']
+            actual_account_specs = [specs['CU_M'],specs['CU_D'], specs['CU_I'], specs['CU_C']]
+            if (response.status_code != 200) or (actual_account_specs != account_specs) :
                 response_data = {'status_code': response.status_code, 'content': response.content}
                 service.model.data.result = 'FAILED : %s %s' % ('test_create_accounts_with_specs',response_data)
                 break
