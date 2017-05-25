6337b21c1c5cb1278bdf075fd4db2502ee7232d4
alichaddad <chaddada@greenitglobe.com>
2017-05-25 13:50:09 +0200

    updated g8osfs for new g8ufs

--- templates/fs/fs.g8osfs/actions.py
+++ templates/fs/fs.g8osfs/actions.py
@@ -3,106 +3,39 @@ def install(job):
 
     cuisine.package.mdupdate()
     cuisine.package.install('fuse')
-    bin_location = '/usr/local/bin/fs'
-    cuisine.core.dir_ensure('/usr/local/bin')
-    cuisine.core.file_download('https://stor.jumpscale.org/public/fs', bin_location)
-    cuisine.core.file_attribs('/usr/local/bin/fs', '0550')
+    cuisine.core.dir_ensure('/bin')
+    cuisine.core.file_download('https://stor.jumpscale.org/public/g8ufs', '/bin/g8ufs')
+    cuisine.core.file_download('https://stor.jumpscale.org/public/lib-dep.tar.gz', '$TMPDIR/lib-dep.tar.gz')
+    cuisine.core.file_attribs('/bin/g8ufs', '0755')
+    cuisine.core.run('tar -zxf $TMPDIR/lib-dep.tar.gz -C /usr/lib --strip-components 1')
 
     service = job.service
     cuisine = service.executor.cuisine
 
-    final_config = {
-        'mount': [],
-        'backend': {},
-        'aydostor': {},
-    }
-
-    targets = []
-
-    for config in service.producers['vfs_config']:
-        # TODO download flist
-        flist_path = cuisine.core.replace('$TMPDIR/%s' % j.sal.fs.getBaseName(config.model.data.mountFlist))
-        cuisine.core.file_download(config.model.data.mountFlist, flist_path, overwrite=True)
-
-        targets.append(config.model.data.mountMountpoint)
-
-        mount = {
-            'path': config.model.data.mountMountpoint,
-            'flist': flist_path,
-            'backend': config.name,
-            'mode': config.model.data.mountMode,
-            'trim_base': config.model.data.mountTrimbase,
-            'trim': config.model.data.mountTrim,
-        }
-
-        cuisine.core.dir_ensure(config.model.data.backendPath)
-        backend = {
-            'path': config.model.data.backendPath,
-            'stor': config.name,
-            'namespace': config.model.data.backendNamespace,
-            'upload': config.model.data.backendUpload,
-            'encrypted': config.model.data.backendEncrypted,
-            'user_rsa': config.model.data.backendUserRsa,
-            'store_rsa': config.model.data.backendStoreRsa,
-            'aydostor_push_cron': config.model.data.backendPush,
-            'cleanup_cron': config.model.data.backendCleanupCron,
-            'cleanup_older_than': config.model.data.backendCleanupOld,
-        }
-
-        store = {
-            'addr': config.model.data.storeUrl,
-            'login': config.model.data.storeLogin,
-            'passwd': config.model.data.storePassword,
-        }
-
-        final_config['mount'].append(mount)
-        final_config['backend'][config.name] = backend
-        final_config['aydostor'][config.name] = store
-
-    # make sure nonthing is already mounted
-    for mount in final_config['mount']:
-        cmd = 'umount -fl %s' % mount['path']
-        cuisine.core.run(cmd, die=False)
-
-    # create all mountpoints but make sure we don't create folder inside mountpoints in the cases
-    # we would have a mountpoint inside another
-    # e.g for two mountpoints:
-    # /mnt/root
-    # /mnt/root/opt
-    # we only create /mnt/root
-    tocreate = {m['path'] for m in final_config['mount']}
-    todelete = set()
-    for path in tocreate:
-        if j.sal.fs.getParent(path) in tocreate:
-            todelete.add(path)
-
-    for path in tocreate.difference(todelete):
-        cuisine.core.dir_ensure(path)
-
-    # write config
-    config_path = cuisine.core.replace('$JSCFGDIR/fs/%s.toml' % service.name)
-    cuisine.core.file_write(config_path, j.data.serializer.toml.dumps(final_config))
-
-    # create service
+    flist_path = cuisine.core.replace('$TMPDIR/%s' % j.sal.fs.getBaseName(service.model.data.flist))
+    cuisine.core.file_download(service.model.data.flist, flist_path, overwrite=True)
+    meta_path = '$TMPDIR/meta'
+    cuisine.core.dir_ensure(meta_path)
+    cuisine.core.run('tar -zxf %s -C %s' % (flist_path, meta_path))
+    cmd = 'umount -fl %s' % service.model.data.mountPoint
+    cuisine.core.run(cmd, die=False)
     pm = cuisine.processmanager.get('tmux')
-    bin_location = cuisine.core.command_location('fs')
-    cmd = '%s -config %s' % (bin_location, config_path)
-    pm.ensure("fs_%s" % service.name, cmd=cmd, env={}, path='$JSCFGDIR/fs', descr='G8OS FS', autostart=True, wait="3m")
+    cmd = 'g8ufs -meta %s -storage-url %s %s' % (meta_path, service.model.data.storageUrl, service.model.data.mountPoint)
+    pm.ensure("fs_%s" % service.name, cmd=cmd, env={}, descr='G8OS FS', autostart=True, wait="3m")
 
     # wait until all targets are actually mounted
     # We wait max 1 min per target
     # NOTE: in real life, once one target is ready all targets would be there
     import time
-    for target in targets:
-        trials = 12
-        while trials > 0:
-            code, _, _ = cuisine.core.run('mount | grep -P "on {}\s"'.format(target), die=False)
-            if code != 0:
-                # not found yet. We sleep for 5 seconds
-                time.sleep(5)
-            else:
-                break
-            trials -= 1
+    trials = 12
+    while trials > 0:
+        code, _, _ = cuisine.core.run('mount | grep -P "on {}\s"'.format(service.model.data.mountPoint), die=False)
+        if code != 0:
+            # not found yet. We sleep for 5 seconds
+            time.sleep(5)
+        else:
+            break
+        trials -= 1
 
 def start(job):
     # the start needs all the steps from install so just re-call install
