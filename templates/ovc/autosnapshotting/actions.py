a59ebe86889061bdb97742a397427a7e736af195
Ahmed T. Youssef <xmonader@gmail.com>
2017-10-09 15:08:39 +0200

    dont autosnapshot if u can't get the cloudspace

--- templates/ovc/autosnapshotting/actions.py
+++ templates/ovc/autosnapshotting/actions.py
@@ -16,27 +16,33 @@ def install(job):
     service.saveAll()
 
 
-def snapshot(job):
-    """Taking snapshot of all machines in the given space if it meets the conditions specified in the schema"""
+def snapshot (job):
     from dateutil import parser
     import datetime
-
+    import time
     service = job.service
     START_DATE = parser.parse(service.model.data.startDate) if service.model.data.endDate != "" else ""
     END_DATE = parser.parse(service.model.data.endDate) if service.model.data.endDate != "" else ""
-
     start_date_valid = START_DATE == "" or START_DATE < datetime.datetime.now()
     end_date_valid = END_DATE == "" or END_DATE > datetime.datetime.now()
-
-    if start_date_valid and end_date_valid:
-        # Get spaces id from the parent (vdc)
-        vdc = service.producers["vdc"][0]
-        # Get given space resides in g8client
-        g8client = vdc.producers["g8client"][0]
-        cl = j.clients.openvcloud.getFromService(g8client)
-        cl = cl.account_get(vdc.model.data.account)
-        space = cl.space_get(vdc.name, vdc.model.data.location)
-        for name, machine in space.machines.items():
+    vdc = service.producers["vdc"][0]
+    g8client = vdc.producers["g8client"][0]
+    cl = j.clients.openvcloud.getFromService(g8client)
+    cl = cl.account_get(vdc.model.data.account)
+    space = None
+    try:
+        space = cl.space_get(vdc.name, vdc.model.data.location, create=False)
+    except:
+        return
+    now = int(time.time())
+    period = j.data.types.duration.convertToSeconds(service.model.data.snapshotInterval)
+    if not (start_date_valid and end_date_valid):
+        return
+    for name, machine in space.machines.items():
+        for snapshot in machine.list_snapshots():
+            delta = now - snapshot['epoch']
+            if delta < period:
+                break
             machine.create_snapshot(name='auto_snapshot')
 
 
